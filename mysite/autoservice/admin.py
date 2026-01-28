2b61c1267a8ee04c0598df56c4715501138329a6
Povilas <pavilunskas@gmail.com>
2026-01-28 08:44:00 +0200

    Update admin registrations

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Service, Car, Order, OrderLine
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = []
+
+admin.site.register(Service)
+admin.site.register(Car)
+admin.site.register(Order)
+admin.site.register(OrderLine)

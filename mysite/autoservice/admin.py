888a26ac586efbe1d771c9e16e39f648af29a722
Povilas <pavilunskas@gmail.com>
2026-01-29 13:24:01 +0200

    Pakeitimai orderlineadmin

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -4,6 +4,13 @@ from .models import Service, Car, Order, OrderLine
 class CarAdmin(admin.ModelAdmin):
     list_display = ['make', 'model', 'license_plate', 'display_vin_code']
 
+class OrderLineAdmin(admin.ModelAdmin):
+    list_display = ['order', 'service', 'quantity', 'line_sum']
+    fieldsets = [
+        ("General", {'fields': ['order', 'service', 'quantity', 'line_sum']}),
+    ]
+    readonly_fields = ['line_sum']
+
 admin.site.register(Car, CarAdmin)
 admin.site.register(Service)
 admin.site.register(Order)

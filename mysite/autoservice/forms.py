13235a20845c905f7848e78bd59f8c02a9ebae10
Povilas <pavilunskas@gmail.com>
2026-02-11 11:55:01 +0200

    Sukuriame formą vartotojo redagavimui.

--- mysite/autoservice/forms.py
+++ mysite/autoservice/forms.py
@@ -1,7 +1,13 @@
 from .models import OrderReview
 from django import forms
+from django.contrib.auth.models import User
 
 class OrderReviewForm(forms.ModelForm):
     class Meta:
         model = OrderReview
         fields = ['content']
+
+class UserChangeForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
